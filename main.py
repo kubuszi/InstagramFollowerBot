6d4dc02b034d13b2ad66358d7e59d9c0354ad605
kubuszi <kubushii2@wp.pl>
2024-06-01 00:17:20 +0200

    Login to Instagram

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
-
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
 
 SIMILAR_ACCOUNT = ""
 USERNAME = ""
@@ -14,7 +16,33 @@ class InstaFollower:
         self.driver = webdriver.Chrome(options=chrome_options)
 
     def login(self):
-        pass
+        url = "https://www.instagram.com/accounts/login/"
+        self.driver.get(url)
+        time.sleep(4.2)
+
+        decline_cookies_xpath = "/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div[2]/div/button[2]"
+        cookie_warning = self.driver.find_elements(By.XPATH, decline_cookies_xpath)
+        if cookie_warning:
+            cookie_warning[0].click()
+        time.sleep(2)
+        username = self.driver.find_element(by=By.NAME, value="username")
+        password = self.driver.find_element(by=By.NAME, value="password")
+
+        username.send_keys(USERNAME)
+        password.send_keys(PASSWORD)
+
+        time.sleep(2.1)
+        password.send_keys(Keys.ENTER)
+
+        time.sleep(4.3)
+        save_login_prompt = self.driver.find_element(by=By.XPATH, value="//div[contains(text(), 'Nie teraz')]")
+        if save_login_prompt:
+            save_login_prompt.click()
+
+        time.sleep(3.7)
+        notifications_prompt = self.driver.find_element(by=By.XPATH, value="// button[contains(text(), 'Nie teraz')]")
+        if notifications_prompt:
+            notifications_prompt.click()
 
     def find_followers(self):
         pass
